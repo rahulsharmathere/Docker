d0af99c195bd6f398c5437e4a25f882f96c1e259
Rahul <rahulsharmadl2004@gmail.com>
2026-07-16 11:34:54 +0530

    phase 2 docker compose

---
+++ level1/phase2dockerCompose/backend/index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const dotenv = require('dotenv')
+
+const app=express();
+
+dotenv.config()
+
+const port=process.env.PORT || 5000
+
+app.get("/",(req,res)=>{
+    return res.status(200).json({
+        message:"Hello from docker PHASE 2"
+    })
+})
+
+app.listen(port,()=>{
+    console.log("server started")
+})
